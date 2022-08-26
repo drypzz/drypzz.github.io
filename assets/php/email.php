2aabaf34ced0942ca0b35def2c1a3bca12f1300b
elseif <fdfjonfmd@gmail.com>
2022-08-26 13:32:21 -0300

    att .php

---
+++ assets/php/email.php
@@ -0,0 +1,21 @@
+<?php
+
+    if( isset($_POST['name']) && isset($_POST['telephone']) && isset($_POST['email']) && isset($_POST['assunto']) && isset($_POST['msg']) ){
+        if(!empty($_POST['email'])){
+            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+
+                $to = 'gugapalmeiraa@gmail.com';
+                $from = 'Contato - @elseif';
+                $body = "Nome: ".addslashes($_POST['name'])."\r\n"."Email: ".addslashes($_POST['email'])."\r\n"."Telefone: ".addslashes($_POST['telephone'])."\r\n"."Assunto: ".addslashes($_POST['assunto'])."\r\n"."Menssagem: ".addslashes($_POST['msg'])."";
+                $header = "From:gugapalmeiraa@gmail.com"."\r\n"."Reply-To:".$_POST['email']."\e\n"."X=Maier:PHP/".phpversion();
+
+                if(mail($to, $from, $body, $header)){
+                    echo 'Email enviado com sucesso.';
+                }else{
+                    echo 'Erro';
+                };
+            };
+        };
+    };
+
+?>
