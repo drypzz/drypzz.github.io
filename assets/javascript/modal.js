6a74092d416f34252ab4f45aff2f8f4956d5b897
elseif <fdfjonfmd@gmail.com>
2022-09-10 21:52:52 -0300

    att gfx/.js/.html

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,11 +1,23 @@
-const open = document.querySelector('#open-details');
-const close = document.querySelector('#close-details');
-const popup = document.querySelector('.popup');
+const open_museuinfo = document.querySelector('#open-details-museuinfo');
+const close_museuinfo = document.querySelector('#close-details-museuinfo');
+const popup_museuinfo = document.querySelector('#museuinfo');
 
-open.addEventListener('click', () => {
-    popup.classList.add('show');
+open_museuinfo.addEventListener('click', () => {
+    popup_museuinfo.classList.add('show');
 });
 
-close.addEventListener('click', () => {
-    popup.classList.remove('show');
+close_museuinfo.addEventListener('click', () => {
+    popup_museuinfo.classList.remove('show');
+});
+
+const open_sresra = document.querySelector('#open-details-sresra');
+const close_sresra = document.querySelector('#close-details-sresra');
+const popup_sresra = document.querySelector('#sresra');
+
+open_sresra.addEventListener('click', () => {
+    popup_sresra.classList.add('show');
+});
+
+close_sresra.addEventListener('click', () => {
+    popup_sresra.classList.remove('show');
 });
